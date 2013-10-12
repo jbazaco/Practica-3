7b259d99d73b0c19bec21550f57de9b487e66589
jbazaco <bazaco15@gmail.com>
2013-10-12 17:53:09 +0200

    reemplazado engine.js del prototipo 08 con el del mismo dentro de src

--- 08-missiles/engine.js
+++ 08-missiles/engine.js
@@ -9,7 +9,7 @@
 // puntuación o el número de vidas.
 
 
-
+var _;
 
 // Objeto singleton Game: se guarda una unica instancia del
 // constructor anónimo en el objeto Game
@@ -208,19 +208,12 @@ var GameBoard = function() {
     this.iterate = function(funcName) {
 	// Convertimos en un array args (1..)
 	var args = Array.prototype.slice.call(arguments,1);
-
-	for(var i=0, len=this.objects.length; i<len;i++) {
-	    var obj = this.objects[i];
-	    obj[funcName].apply(obj,args)
-	}
+	_(this.objects).each( function(obj) { obj[funcName].apply(obj,args) });
     };
 
     // Devuelve el primer objeto de objects para el que func es true
     this.detect = function(func) {
-	for(var i = 0,val=null, len=this.objects.length; i < len; i++) {
-	    if(func.call(this.objects[i])) return this.objects[i];
-	}
-	return false;
+	return _(this.objects).find( function(obj) { return func.call(obj) }) || false;
     };
 
     // Cuando Game.loop() llame a step(), hay que llamar al método
